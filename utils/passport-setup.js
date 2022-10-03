cd5f454f5270fa4091500e9757b1bd31fd773b45
rajan chouhan <rajan.chouhan25@gmail.com>
2022-10-04 01:31:03 +0530

    changed call back url of oauth20

--- utils/passport-setup.js
+++ utils/passport-setup.js
@@ -7,7 +7,7 @@ const GoogleStrategy=require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: "205571255181-5ov7unf92jg13pqeoj745m3kq1aopbra.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: 'http://localhost:3000/auth/google/callback'},async (accessToken, refreshToken, profile, cb)=>{
+    callbackURL: 'https://codecamp-intern.herokuapp.com/auth/google/callback'},async (accessToken, refreshToken, profile, cb)=>{
         const user=await User.find({email:profile._json.email});
         return cb(null,user);
         
